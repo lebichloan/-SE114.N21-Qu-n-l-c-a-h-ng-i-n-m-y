f63ed6073a40ab5c5d3a98bcac78c62e15955b71
Bích Loan <106142309+lebichloan@users.noreply.github.com>
2023-03-21 15:03:39 +0700

    gâu gâu meo meo

--- app/src/main/java/com/example/se114n21/MainActivity.java
+++ app/src/main/java/com/example/se114n21/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.se114n21;
 
-import android.content.Intent;
-import android.support.v4.app.SupportActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.example.se114n21.reports.report_activity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // comment
-
-        Intent rpIntent = new Intent(MainActivity.this, report_activity.class);
-        startActivity(rpIntent);
-
 
     }
 }
