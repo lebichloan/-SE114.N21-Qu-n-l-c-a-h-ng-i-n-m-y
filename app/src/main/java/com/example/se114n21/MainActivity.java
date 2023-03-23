9d644dc91701fca5901f29426c5359fb32079e29
@sangquangtryhard <sang7b7b@gmail.com>
2023-03-23 14:32:49 +0700

    test something big 1

--- app/src/main/java/com/example/se114n21/MainActivity.java
+++ app/src/main/java/com/example/se114n21/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // comment
-
+        System.out.println("Hello Bich Loan");
 
     }
 }
