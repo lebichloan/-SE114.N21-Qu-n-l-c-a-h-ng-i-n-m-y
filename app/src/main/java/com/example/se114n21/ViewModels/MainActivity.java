1068eadf486258e76e0ec3ac372e1997ccacb4cf
BichLoan <21521083@gm.uit.edu.vn>
2023-05-17 21:17:31 +0700

    ...

--- app/src/main/java/com/example/se114n21/ViewModels/MainActivity.java
+++ app/src/main/java/com/example/se114n21/ViewModels/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.se114n21.ViewModels;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.se114n21.R;
 
@@ -12,9 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getDataIntent();
         setTitleToolbar();
     }
 
+    private void getDataIntent() {
+        String strPhoneNumber = getIntent().getStringExtra("phone_number");
+
+        TextView tvUserInfor = findViewById(R.id.tv_user_infor);
+        tvUserInfor.setText(strPhoneNumber);
+    }
     private void setTitleToolbar() {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("Main Activity");
