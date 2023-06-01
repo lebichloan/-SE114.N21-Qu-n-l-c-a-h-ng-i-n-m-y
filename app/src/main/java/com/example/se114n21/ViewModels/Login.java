44b3add83c5290105c2dc36fad58b62b75afbc17
BichLoan <21521083@gm.uit.edu.vn>
2023-06-01 08:19:49 +0700

    login+forgotpassword

--- app/src/main/java/com/example/se114n21/ViewModels/Login.java
+++ app/src/main/java/com/example/se114n21/ViewModels/Login.java
@@ -1,16 +1,89 @@
 package com.example.se114n21.ViewModels;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.se114n21.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
+    EditText txtEmail, txtPassword;
+    Button butLogin;
+    FirebaseAuth auth;
+    ProgressBar processBar;
+    TextView textViewForgotPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPassword = findViewById(R.id.txtPassword);
+        butLogin = findViewById(R.id.butLogin);
+        textViewForgotPassword = findViewById((R.id.textViewForgotPassword));
+
+        butLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+                email = String.valueOf(txtEmail.getText());
+                password = String.valueOf(txtPassword.getText());
+
+                if (! email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    if (! password.isEmpty()) {
+                        auth.signInWithEmailAndPassword(email, password)
+                                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                    @Override
+                                    public void onSuccess(AuthResult authResult) {
+                                        Toast.makeText(Login.this, "Login Sucessful", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(Login.this, MainActivity.class));
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(Login.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        txtPassword.setError("Password canot be empty");
+                    }
+                } else if (email.isEmpty()) {
+                    txtEmail.setError("Email cannot be empty");
+                } else {
+                    txtEmail.setError("Please enter valid email");
+                }
+
+            }
+        });
+        textViewForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, ForgotPassword.class));
+                finish();
+            }
+        });
     }
+
 }
+
---
+++ app/src/main/java/com/example/se114n21/ViewModels/SendEmailSucess.java
@@ -0,0 +1,33 @@
+package com.example.se114n21.ViewModels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.example.se114n21.R;
+
+public class SendEmailSucess extends AppCompatActivity {
+
+    Button butBack;
+    ProgressBar processBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_email_sucess);
+
+        butBack = findViewById(R.id.butBack);
+
+        butBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SendEmailSucess.this, Login.class));
+                finish();
+            }
+        });
+    }
+}
