6a214c77df5b852e18673ee5e5fb360792dd77c2
kit <kietphamkb2@gmail.com>
2023-07-17 23:11:35 +0700

    commit to checkout

--- app/src/main/java/com/example/se114n21/ViewModels/AddStaff.java
+++ app/src/main/java/com/example/se114n21/ViewModels/AddStaff.java
@@ -44,9 +44,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -169,7 +171,19 @@ public class AddStaff extends AppCompatActivity {
     }
 
     private void setStaffId() {
+        DatabaseReference myRef = database.getReference("maxNhanVien");
 
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private boolean checkForm() {
