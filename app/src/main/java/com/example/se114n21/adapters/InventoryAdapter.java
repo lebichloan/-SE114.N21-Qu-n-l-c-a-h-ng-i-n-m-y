cb9e0e0109a2b51e7ac8c559889c5db01ce000e2
@sangquangtryhard <sang7b7b@gmail.com>
2023-07-23 15:49:44 +0700

    fix repo

---
+++ app/src/main/java/com/example/se114n21/adapters/InventoryAdapter.java
@@ -0,0 +1,71 @@
+package com.example.se114n21.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+
+import com.example.se114n21.R;
+import com.example.se114n21.models.Inventory;
+
+import java.util.List;
+
+public class InventoryAdapter extends RecyclerView.Adapter<InventoryAdapter.InventoryViewHolder> {
+
+    private Context context;
+    private List<Inventory> listInventory;
+
+    public InventoryAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void setData(List<Inventory> list)
+    {
+        listInventory = list;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public InventoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_repo_inventory, parent, false);
+        return new InventoryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InventoryViewHolder holder, int position) {
+        Inventory inventory = listInventory.get(position);
+        if (inventory == null)
+            return;
+
+        holder.tvName.setText(inventory.getName());
+        holder.tvImport.setText("SL nhập: " + inventory.getImp());
+        holder.tvExport.setText("SL xuất: " + inventory.getExp());
+    }
+
+    @Override
+    public int getItemCount() {
+        if (listInventory != null)
+            return listInventory.size();
+        return 0;
+    }
+
+    public class InventoryViewHolder extends ViewHolder {
+
+        private TextView tvName, tvImport, tvExport;
+
+        public InventoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvName = itemView.findViewById(R.id.tvNameInventory);
+            tvImport = itemView.findViewById(R.id.tvImport);
+            tvExport = itemView.findViewById(R.id.tvExport);
+
+        }
+    }
+}
--- app/src/main/java/com/example/se114n21/reports/repo_inventory.java
+++ app/src/main/java/com/example/se114n21/reports/repo_inventory.java
@@ -17,8 +17,8 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.example.se114n21.R;
-import com.example.se114n21.adapters.StockAdapter;
-import com.example.se114n21.models.Stock;
+import com.example.se114n21.adapters.InventoryAdapter;
+import com.example.se114n21.models.Inventory;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,15 +33,11 @@ import java.util.List;
 
 public class repo_inventory extends Fragment {
 
-    TextView tvDate, tvDate1;
-    Button btnDate, btnDate1;
-    RecyclerView rvStock;
-    List<Stock> stockList;
-    StockAdapter adapter;
-    Calendar calendar = Calendar.getInstance();
-    int year = calendar.get(Calendar.YEAR);
-    int month = calendar.get(Calendar.MONTH);
-    int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+    RecyclerView rvInventory;
+    List<Inventory> inventoryList;
+    InventoryAdapter adapter;
+
 
 
 
@@ -59,95 +55,48 @@ public class repo_inventory extends Fragment {
 
     private void initUI(View view)
     {
-        rvStock = view.findViewById(R.id.rvStock);
-
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        String today = sdf.format(calendar.getTime());
-
-        tvDate = view.findViewById(R.id.tvDate);
-        tvDate1 = view.findViewById(R.id.tvDate1);
-        tvDate.setText(today);
-        tvDate1.setText(today);
-
-        btnDate = view.findViewById(R.id.btnDate);
-        btnDate1 = view.findViewById(R.id.btnDate1);
+        rvInventory = view.findViewById(R.id.rvStock);
     }
 
     private void initEvent()
     {
         //set up data for recycle view
         LinearLayoutManager manager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
-        rvStock.setLayoutManager(manager);
+        rvInventory.setLayoutManager(manager);
 
         DividerItemDecoration divider = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
-        rvStock.addItemDecoration(divider);
+        rvInventory.addItemDecoration(divider);
 
-        stockList = new ArrayList<>();
-        getDataRealTime((String) tvDate.getText(), (String) tvDate1.getText());
+        inventoryList = new ArrayList<>();
+        getDataRealTime();
 
-        adapter = new StockAdapter(getContext());
-        adapter.setData(stockList);
+        adapter = new InventoryAdapter(getContext());
+        adapter.setData(inventoryList);
 
-        rvStock.setAdapter(adapter);
+        rvInventory.setAdapter(adapter);
 
-        //set datePicker for button
-        btnDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DatePickerDialog datePicker = createDatePicker(tvDate);
-                datePicker.show();
-            }
-        });
-
-        btnDate1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DatePickerDialog datePicker = createDatePicker(tvDate1);
-                datePicker.show();
-            }
-        });
     }
 
-    private DatePickerDialog createDatePicker(TextView tv)
+
+    private void getDataRealTime()
     {
-        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int Selectedyear, int Selectedmonth, int SelecteddayOfMonth) {
-                // Xử lý sự kiện khi người dùng chọn ngày
-                calendar.set(Calendar.YEAR, Selectedyear);
-                calendar.set(Calendar.MONTH, Selectedmonth);
-                calendar.set(Calendar.DAY_OF_MONTH, SelecteddayOfMonth);
-
-                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-                String selectedDate = sdf.format(calendar.getTime());
-
-                tv.setText(selectedDate);
-                getDataRealTime((String) tvDate.getText(), (String) tvDate1.getText());
-            }
-        }, year, month, dayOfMonth);
-        return datePickerDialog;
-    }
-
-    private void getDataRealTime(String dateStart, String dateEnd)
-    {
-        List<Stock> list = new ArrayList<>();
+        List<Inventory> list = new ArrayList<>();
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference ref = database.getReference("stock");
+        DatabaseReference ref = database.getReference("inventory");
 
-        Query query = ref.orderByChild("date");
+        Query query = ref.orderByChild("id");
 
         query.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (stockList != null)
-                    stockList.clear();
+                if (inventoryList != null)
+                    inventoryList.clear();
 
                 for (DataSnapshot dataSnapshot : snapshot.getChildren())
                 {
-                    Stock stock = dataSnapshot.getValue(Stock.class);
-                    if (stock.getDate().compareTo(dateStart) >= 0 && stock.getDate().compareTo(dateEnd) <= 0)
-                        stockList.add(stock);
+                    Inventory inventory = dataSnapshot.getValue(Inventory.class);
+                        inventoryList.add(inventory);
                 }
                 adapter.notifyDataSetChanged();
             }
