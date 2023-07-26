98f14449307938a6e59641f76446507507b1411c
kit <kietphamkb2@gmail.com>
2023-07-26 23:44:31 +0700

    final

--- app/src/main/java/com/example/se114n21/listener/IOnManagerStaffListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.se114n21.listener;
-
-import com.example.se114n21.Models.NhanVien;
-
-public interface IOnManagerStaffListener {
-    void onClickUpdateStaff(NhanVien nhanVien);
-    void onClickDeleteStaff(NhanVien nhanVien);
-}
