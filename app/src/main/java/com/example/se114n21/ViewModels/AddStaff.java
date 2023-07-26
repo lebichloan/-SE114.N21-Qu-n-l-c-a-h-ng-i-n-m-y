4bc5c6f43885e220e2d336b3fad7d9bb232d63ee
lebichloan <lebichloan2512@gmail.com>
2023-07-26 22:04:31 +0700

    ...

--- app/src/main/java/com/example/se114n21/ViewModels/AddStaff.java
+++ app/src/main/java/com/example/se114n21/ViewModels/AddStaff.java
@@ -232,13 +232,13 @@ public class AddStaff extends AppCompatActivity {
 
         TextView txtAlert = dialogViewFail.findViewById(R.id.txtAlert);
         txtAlert.setText(data);
-        Button butOK = dialogViewFail.findViewById(R.id.butOK);
-        butOK.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
+//        Button butOK = dialogViewFail.findViewById(R.id.butOK);
+//        butOK.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                dialog.dismiss();
+//            }
+//        });
 
         Window dialogWindow = dialog.getWindow();
         if (dialogWindow != null) {
